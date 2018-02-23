1acacb83c9469c322a03ce1c6bf673da32650827
Robert W <robwash@gmail.com>
2018-02-23 00:25:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@num_of_songs = 0
-  
+
+    def initialize(name, artist, genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@num_of_songs += 1
+
+
+    end
 
 
 
